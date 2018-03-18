cedd2440443ebcd24f4f022cbe94a5525bb784c3
Will Worth <will.worth@gmail.com>
2018-03-18 10:54:29 +0100

    first commit

---
+++ lumdb/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Welcome text = "Welcome to learning props"/> 
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+class Welcome extends Component {
+  render(){
+    return (
+      <h1 className="App-title">{this.props.text}</h1>
+            )
+          }
+}
+
+
+export default App;
