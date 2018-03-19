c040314aa46805e5f90b8eec4b7095f572536c8f
Will Worth <will.worth@gmail.com>
2018-03-19 13:05:02 +0100

    to blank for actual

--- lumdb/src/App.js
+++ lumdb/src/App.js
@@ -3,47 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {
-    input: 'Hello'
-  }
-
-  updateInput = (event) => {
-    this.setState({
-      input: event.target.value.trim()
-    })
-  }
-
-  submit = () => {
-    console.log(this.text.value)
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text="Welcome to Using Props" />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h3>{this.state.input}</h3>
-        <input type="text" onChange={this.updateInput} value={this.state.input}  />
-        <input type="text" ref={(input) => this.text = input } />
-        <button onClick={this.submit}>Show Value</button>
       </div>
     );
   }
 }
-
-class Welcome extends Component {
-  render() {
-    const { text } = this.props;
-    return (
-      <h1 className="App-title">{text}</h1>
-    )
-  }
-}
-
 
 export default App;
