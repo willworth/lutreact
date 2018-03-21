c788a983adfb4bf77fd3102913a3d83f58c4ec34
Will Worth <will.worth@gmail.com>
2018-03-21 08:36:28 +0100

    create Movie component

--- donotuse.js
+++ donotuse.js
@@ -48,3 +48,49 @@ class App extends Component {
   
   export default App;
   
+
+
+
+  // ====================NEW=============
+
+
+
+
+
+  import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars'
+  },  {
+    id: 2,
+    title: 'Spider Mand'
+  },  {
+    id: 3,
+    title: 'lord of the Rings'
+  }
+]; 
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        {movies.map((movie)=>{
+          return(
+            <div key={movie.id}>
+              {movie.title}
+            </div>
+          )
+         })}
+      </div>
+    );
+  }
+}
+
+export default App;
--- lumdb/src/App.js
+++ lumdb/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from './Movie'
 const movies = [
   {
     id: 1,
     title: 'Star Wars'
   },  {
     id: 2,
-    title: 'Spider Mand'
+    title: 'Spider Man'
   },  {
     id: 3,
     title: 'lord of the Rings'
@@ -22,16 +23,13 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map((movie)=>{
-          return(
-            <div key={movie.id}>
-              {movie.title}
-            </div>
-          )
-         })}
+        {movies.map(movie=>(
+            <Movie key={movie.id} movie={movie}/>
+          ))}
       </div>
     );
   }
 }
 
 export default App;
+ 
