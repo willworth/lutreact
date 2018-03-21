54fdd60b7de3b55a22f39b74a85cad1638448dc6
Will Worth <will.worth@gmail.com>
2018-03-21 12:43:30 +0100

    add mdb api and json query

--- lumdb/src/App.js
+++ lumdb/src/App.js
@@ -18,6 +18,21 @@ const movies = [
 ]; 
 
 class App extends Component {
+
+ async componentDidMount() {
+    try{
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=1f010524f483d8b9090cb965239b815a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      console.log(movies)
+
+    }   catch(e){
+      console.log(e);
+
+    }
+  }
+
+
+
   render() {
     return (
       <div className="App">
