11420b68023212bfcd981bee70ae8876cbe9d824
Will Worth <will.worth@gmail.com>
2018-03-24 19:32:20 +0100

    add routing

--- lumdb/src/App.js
+++ lumdb/src/App.js
@@ -3,44 +3,27 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
 } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-import Movie from './Movie';
+import Movieslist from './MoviesList';
 
-class App extends Component {
-  state = {
-    movies: [],
-  }
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <Switch>
+        <Route path="/test" component={Test} />
+        <Route path="/" component={Movieslist} />
+      </Switch>
+    </div>
+  </Router>
+);
 
-  async componentDidMount() {
-    try {
-      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=65e043c24785898be00b4abc12fcdaae&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
-      const movies = await res.json();
-      this.setState({
-        movies: movies.results,
-      });
-    } catch (e) {
-      // eslint-disable-next-line
-      console.log(e);
-    }
-  }
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-          <Route path="/test" component={Test} />
-          {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
-        </div>
-      </Router>
-    );
-  }
-}
 
 export default App;
 
