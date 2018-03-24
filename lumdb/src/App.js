f96eaa3dc971a3d14949d39237a7ec7985cd7c13
Will Worth <will.worth@gmail.com>
2018-03-24 17:57:25 +0100

    reset to tuts

--- lumdb/src/App.js
+++ lumdb/src/App.js
@@ -1,32 +1,26 @@
+/* eslint react/no-did-mount-set-state: 0 */
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Movie from './Movie'
+import Movie from './Movie';
 
 class App extends Component {
-
-  state ={
-    movies:[]
+  state = {
+    movies: [],
   }
 
-
- async componentDidMount() {
-    try{
-      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=1f010524f483d8b9090cb965239b815a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=65e043c24785898be00b4abc12fcdaae&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
       const movies = await res.json();
-      console.log(movies)
       this.setState({
-        movies: movies.results
-      })
-
-    }   catch(e){
+        movies: movies.results,
+      });
+    } catch (e) {
       console.log(e);
-
     }
   }
-
-
 
   render() {
     return (
@@ -34,11 +28,10 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {this.state.movies.map(movie=> <Movie key={movie.id} movie={movie} overview={movie.overview} />   )}
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
 }
 
 export default App;
- 
--- lumdb/src/Movie.js
+++ lumdb/src/Movie.js
@@ -1,22 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Movie extends Component {
-    static propTypes = {
-        movie: PropTypes.shape({
-            title: PropTypes.string.isRequired,
-            overview: PropTypes.string.isRequired
-        }),
-    }
-// CANNOT HAVE NESTED DEFAULT PROPS!!!!!!!!!
- 
+const Movie = ({ movie }) => (
+  <div>
+    <h3>{movie.title}</h3>
+  </div>
+);
 
-    render() {
-        return(
-            <div>
-           <h3>{this.props.movie.title}</h3>
-           <h2>{this.props.movie.overview}</h2>
-            </div>
-        )
-    }
-}
+export default Movie;
+
+Movie.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+};
