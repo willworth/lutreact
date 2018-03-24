6502297263ff561fdd736312d78651c6e9ec3aae
Will Worth <will.worth@gmail.com>
2018-03-24 19:46:49 +0100

    add dynamic params!

--- lumdb/src/Movie.js
+++ lumdb/src/Movie.js
@@ -8,7 +8,7 @@ const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 const Movie = ({ movie }) => (
   <div>
     {/* <h3>{movie.title}</h3> */}
-    <Link to="/test">
+    <Link to={`/${movie.id}`}>
       <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
     </Link>
   </div>
